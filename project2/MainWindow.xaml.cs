95ab713150133218ab206077790c5102c8a23f55
Vinayak <Vinayak@WINDOWS-0UPS2DC>
2024-06-22 00:04:31 -0700

    Add project files.

---
+++ project2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace project2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Find the MenuBar control and subscribe to the ImageSelected event
+            MenuBarControl.ImageSelected += MenuBarControl_ImageSelected;
+        }
+
+        private void MenuBarControl_ImageSelected(string filePath)
+        {
+            // Create a BitmapImage and set it as the source of the main Image control
+            var image = new BitmapImage(new Uri(filePath));
+            MainImage.Source = image;
+        }
+    }
+}
---
+++ project2/view/usercontrols/MenuBar.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project2.view.usercontrols
+{
+    public partial class MenuBar : UserControl
+    {
+        // Event to notify subscribers when an image is selected
+        public event Action<string> ImageSelected;
+
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open_file_dialog = new OpenFileDialog();
+            open_file_dialog.Filter = "Image files|*.jpg;*.jpeg;*.png";
+            open_file_dialog.FilterIndex = 1;
+            if (open_file_dialog.ShowDialog() == true)
+            {
+                // Raise the event with the selected file path
+                ImageSelected?.Invoke(open_file_dialog.FileName);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+
+
+        {
+
+          MessageBoxResult result = MessageBox.Show("Do you want to exit ?","Close" , MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
